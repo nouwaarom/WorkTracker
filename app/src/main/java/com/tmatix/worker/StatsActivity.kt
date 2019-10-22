1a24cb4f140e5237887852a55112759e60390262
Elbert van de Put <elbert@amber.team>
2019-10-22 12:40:19 +0200

    results: Add a clear results button.

--- app/src/main/java/com/tmatix/worker/StatsActivity.kt
+++ app/src/main/java/com/tmatix/worker/StatsActivity.kt
@@ -1,54 +1,52 @@
 package com.tmatix.worker
 
-import java.util.ArrayList
-import java.util.Random
-
 import android.app.Activity
 import android.os.Bundle
 
-import android.content.SharedPreferences
 import android.graphics.Color
+import android.widget.Button
 
 import com.androidplot.pie.PieChart
 import com.androidplot.pie.Segment
 import com.androidplot.pie.SegmentFormatter
+import kotlin.random.Random
 
 /**
  * @author Elbert van de Put <elbert@amber.team>
  */
 class StatsActivity : Activity() {
-
-    //list of segments for the pie chart
-    internal var segments: MutableList<Segment> = ArrayList()
-    internal var formatter: MutableList<SegmentFormatter> = ArrayList()
-
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
 
-        val pie = findViewById(R.id.simplePieChart) as PieChart
+        val clearButton = findViewById<Button>(R.id.clearButton)
+        clearButton.setOnClickListener {
+            val timeStorage = getSharedPreferences("nouwaarom_worker_prefs", 0)
+            timeStorage.edit().clear().apply()
 
+            val pie = findViewById<PieChart>(R.id.simplePieChart)
+            fillPieChart(mapOf(), pie)
+        }
+
+        val pie = findViewById<PieChart>(R.id.simplePieChart)
         val timeStorage = getSharedPreferences("nouwaarom_worker_prefs", 0)
+        fillPieChart(timeStorage.all, pie)
+    }
 
-        val rnd = Random()
-
-        //get all stored times
-        val keys = timeStorage.all
-
+    fun fillPieChart(keys: Map<String, Any?>, pie: PieChart) {
+        pie.clear()
         //iterate over the stored times
         for ((key, value) in keys) {
             //add the time to the pie
-            segments.add(Segment(key, value as Int))
-            formatter.add(SegmentFormatter(null))
-
-            //get the last item of the list and set the random color
-            formatter[formatter.size - 1].fillPaint.color = Color.HSVToColor(floatArrayOf(rnd.nextFloat()*360, 1.0f, 0.8f))
+            val segment = Segment(key, value as Int)
+            val formatter = SegmentFormatter(Color.HSVToColor(floatArrayOf(Random.nextFloat()*360, 1.0f, 0.8f)))
 
             //add the segment to the pie
-            pie.addSeries(segments[segments.size - 1], formatter[segments.size - 1])
+            pie.addSeries(segment, formatter)
         }
 
         pie.borderPaint.color = Color.TRANSPARENT
         pie.backgroundPaint.color = Color.TRANSPARENT
+        pie.redraw()
     }
 }
